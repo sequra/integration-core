7019f0059f6b9ce489c3789c8f029a243486cba2
Mikel Martin <mikel.martin@sequra.es>
2024-06-13 09:40:17 +0200

    410 add support for selling services (#11)
    * Add phpunit workflow
    
    * Set php version
    
    * Add workflow and PR template
    
    Co-authored-by: Mikel Martin <mikel.martin@sequra.es>
    
    * Format code with phpcbf and add phpcs and phpunit to workflow
    
    * Add new properties to GeneralSettings entity and model
    
    * Add .code-workspace to .gitignore
    
    * Add new properties to GeneralSettingsResponse and GeneralSettingsRequest classes
    
    * Set default values in GeneralSettingsRequest constructor for new attributes
    
    * Update default values for allowFirstServicePaymentDelay and allowServiceRegItems in GeneralSettingsRequest constructor
    
    ---------
    
    Co-authored-by: Michel Escalante Álvarez <michel.escalante@sequra.es>

--- src/BusinessLogic/AdminAPI/GeneralSettings/Requests/GeneralSettingsRequest.php
+++ src/BusinessLogic/AdminAPI/GeneralSettings/Requests/GeneralSettingsRequest.php
@@ -38,24 +38,56 @@ class GeneralSettingsRequest extends Request
     private $excludedProducts;
 
     /**
+     * @var bool
+     */
+    private $enabledForServices;
+
+    /**
+     * @var bool
+     */
+    private $allowFirstServicePaymentDelay;
+
+    /**
+     * @var bool
+     */
+    private $allowServiceRegItems;
+
+    /**
+     * @var string
+     */
+    private $defaultServicesEndDate;
+
+    /**
      * @param bool $sendOrderReportsPeriodicallyToSeQura
      * @param bool|null $showSeQuraCheckoutAsHostedPage
      * @param string[]|null $allowedIPAddresses
      * @param string[]|null $excludedProducts
      * @param string[]|null $excludedCategories
+     * @param bool $enabledForServices Default value is false
+     * @param bool $allowFirstServicePaymentDelay Default value is false
+     * @param bool $allowServiceRegItems Default value is false
+     * @param string $defaultServicesEndDate Default value is 'P1Y'
      */
     public function __construct(
         bool $sendOrderReportsPeriodicallyToSeQura,
         ?bool $showSeQuraCheckoutAsHostedPage,
         ?array $allowedIPAddresses,
         ?array $excludedProducts,
-        ?array $excludedCategories
+        ?array $excludedCategories,
+        bool $enabledForServices = false,
+        bool $allowFirstServicePaymentDelay = false,
+        bool $allowServiceRegItems = false,
+        string $defaultServicesEndDate = 'P1Y'
     ) {
         $this->sendOrderReportsPeriodicallyToSeQura = $sendOrderReportsPeriodicallyToSeQura;
         $this->showSeQuraCheckoutAsHostedPage = $showSeQuraCheckoutAsHostedPage;
         $this->allowedIPAddresses = $allowedIPAddresses;
         $this->excludedProducts = $excludedProducts;
         $this->excludedCategories = $excludedCategories;
+        $this->enabledForServices = $enabledForServices;
+        $this->allowFirstServicePaymentDelay = $allowFirstServicePaymentDelay;
+        $this->allowServiceRegItems = $allowServiceRegItems;
+        $this->defaultServicesEndDate = $defaultServicesEndDate;
     }
 
     /**
@@ -70,7 +102,11 @@ class GeneralSettingsRequest extends Request
             $this->showSeQuraCheckoutAsHostedPage,
             $this->allowedIPAddresses,
             $this->excludedProducts,
-            $this->excludedCategories
+            $this->excludedCategories,
+            $this->enabledForServices,
+            $this->allowFirstServicePaymentDelay,
+            $this->allowServiceRegItems,
+            $this->defaultServicesEndDate
         );
     }
 }
