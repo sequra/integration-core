056cdced66182db77b72337e5b152f685f577b5f
Mikel Martin <mikel.martin@sequra.es>
2024-06-13 09:51:06 +0200

    Feature/par 415 extend widget settings (#12)
    * Add phpunit workflow
    
    * Set php version
    
    * Add workflow and PR template
    
    Co-authored-by: Mikel Martin <mikel.martin@sequra.es>
    
    * Format code with phpcbf and add phpcs and phpunit to workflow
    
    * Add widget location configuration
    
    * Update WidgetSettings request and response implementation to include new attributes
    
    * chore: Update WidgetSettings request and response implementation
    
    ---------
    
    Co-authored-by: Michel Escalante Álvarez <michel.escalante@sequra.es>
    Co-authored-by: Michel Escalante Álvarez <michel.escalante@sequra.com>
    
    Previous commit: 65084e3c577a10a350a9bd35094a1073a9dba6e0

---
+++ src/BusinessLogic/Domain/PromotionalWidgets/Models/WidgetLocation.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace SeQura\Core\BusinessLogic\Domain\PromotionalWidgets\Models;
+
+/**
+ * Class WidgetLocation
+ *
+ * @package SeQura\Core\BusinessLogic\Domain\PromotionalWidgets\Models
+ */
+class WidgetLocation
+{
+    /**
+     * CSS selector for retrieving the element where the widget should be inserted.
+     *
+     * @var string
+     */
+    private $selForTarget;
+
+     /**
+     * The seQura product identifier.
+     *
+     * @var string
+     */
+    private $product;
+
+     /**
+     * The country identifier.
+     *
+     * @var string
+     */
+    private $country;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(string $selForTarget, string $product = null, string $country = null)
+    {
+        $this->selForTarget = $selForTarget;
+        $this->product = $product;
+        $this->country = $country;
+    }
+
+    public function getSelForTarget(): string
+    {
+        return $this->selForTarget;
+    }
+
+    public function getProduct(): string
+    {
+        return $this->product;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function setSelForTarget(string $selForTarget): void
+    {
+        $this->selForTarget = $selForTarget;
+    }
+
+    public function setProduct(string $product): void
+    {
+        $this->product = $product;
+    }
+
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    public static function fromArray(array $data): ?self
+    {
+        if (!isset($data['selForTarget'])) {
+            return null;
+        }
+
+        return new self(
+            $data['selForTarget'],
+            isset($data['product']) ? $data['product'] : null,
+            isset($data['country']) ? $data['country'] : null
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'selForTarget' => $this->selForTarget,
+            'product' => $this->product,
+            'country' => $this->country
+        ];
+    }
+
+    public function isDefaultLocation(): bool
+    {
+        return $this->product === null && $this->country === null;
+    }
+}
