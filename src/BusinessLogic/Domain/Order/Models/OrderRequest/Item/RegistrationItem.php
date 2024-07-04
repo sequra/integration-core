f4ec20d85d9755e3f3dd59f1f34771ed5dbaf09f
Mikel Martin <mikel.martin@sequra.es>
2024-07-04 11:12:35 +0200

    Add registration type items for carts

---
+++ src/BusinessLogic/Domain/Order/Models/OrderRequest/Item/RegistrationItem.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace SeQura\Core\BusinessLogic\Domain\Order\Models\OrderRequest\Item;
+
+/**
+ * Class RegistrationItem
+ *
+ * @package SeQura\Core\BusinessLogic\Domain\Order\Models\OrderRequest\Item
+ */
+class RegistrationItem extends Item
+{
+    /**
+     * @var string|int A unique code that refers to this registration.
+     */
+    private $reference;
+
+    /**
+     * @var string A name to describe this registration.
+     */
+    private $name;
+
+    /**
+     * @param int|string $reference
+     * @param string $name
+     * @param int $totalWithTax
+     */
+    public function __construct($reference, string $name, int $totalWithTax)
+    {
+        parent::__construct($totalWithTax, ItemType::TYPE_REGISTRATION);
+
+        $this->reference = $reference;
+        $this->name = $name;
+    }
+
+    /**
+     * Create RegistrationItem object from array.
+     *
+     * @param array $data
+     *
+     * @return RegistrationItem
+     */
+    public static function fromArray(array $data): Item
+    {
+        $totalWithTax = self::getDataValue($data, 'total_with_tax', 0);
+        $reference = self::getDataValue($data, 'reference');
+        $name = self::getDataValue($data, 'name');
+
+        return new self($reference, $name, $totalWithTax);
+    }
+
+    /**
+     * @return int|string
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        return $this->transformPropertiesToAnArray(get_object_vars($this));
+    }
+}
