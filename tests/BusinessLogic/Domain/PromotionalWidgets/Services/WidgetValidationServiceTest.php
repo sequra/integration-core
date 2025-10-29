61ce120629ebe9ea102c318bfa71b0d3f302c7b8
Michel Escalante Álvarez <michel.escalante@sequra.es>
2025-10-29 18:00:15 +0100

    Bugfix/PAR-646 widget is not showing for virtual products (#44)
    * Refactor product support validation logic in WidgetValidationService
    
    * Update tests
    
    * Fix data provider naming in WidgetValidationServiceTest for consistency

--- tests/BusinessLogic/Domain/PromotionalWidgets/Services/WidgetValidationServiceTest.php
+++ tests/BusinessLogic/Domain/PromotionalWidgets/Services/WidgetValidationServiceTest.php
@@ -190,18 +190,56 @@ class WidgetValidationServiceTest extends BaseTestCase
     }
 
     /**
+     * @dataProvider dataProviderIsProductSupportedForVirtualProduct
      * @return void
      */
-    public function testIsProductSupportedForVirtualProduct(): void
+    public function testIsProductSupportedForVirtualProduct(?GeneralSettings $generalSettings, bool $expected): void
     {
         //arrange
+        $this->mockGeneralSettingsService->saveGeneralSettings($generalSettings);
+        $this->mockProductService->setMockProductSku('sku1');
         $this->mockProductService->setMockProductVirtual(true);
 
         // act
         $result = $this->widgetValidationService->isProductSupported('sku1');
 
         // assert
-        self::assertFalse($result);
+        self::assertEquals($expected, $result);
+    }
+
+    public function dataProviderIsProductSupportedForVirtualProduct(): array
+    {
+        return [
+            // No general settings
+            [
+                null,
+                true
+            ],
+            // No service selling enabled
+            [
+                new GeneralSettings(
+                    true,
+                    null,
+                    [],
+                    [],
+                    []
+                ),
+                false
+            ],
+            // Service selling enabled
+            [
+                new GeneralSettings(
+                    true,
+                    null,
+                    [],
+                    [],
+                    [],
+                    ['ES']
+                ),
+                true
+            ],
+
+        ];
     }
 
     /**
